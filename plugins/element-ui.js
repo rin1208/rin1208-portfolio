0aba44ae36efb50149a883fcb34d6703ae179291
Takuya Sugiyama <t.sugiyama@jxpress.net>
2019-12-17 10:42:27 +0900

    rin1208

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -2,11 +2,7 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 import "vue-awesome/icons";
 import Icon from "vue-awesome/components/Icon";
-import SimpleTimeline from "vue-simple-timeline";
-import { Slide } from "vue-burger-menu";
 
-Vue.component("slide", Slide);
-Vue.use(SimpleTimeline);
 Vue.component("v-icon", Icon);
 const locale = require("element-ui/lib/locale/lang/ja");
 Vue.use(ElementUI, { locale });
---
+++ plugins/timeline.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import SimpleTimeline from "vue-simple-timeline";
+Vue.use(SimpleTimeline);
